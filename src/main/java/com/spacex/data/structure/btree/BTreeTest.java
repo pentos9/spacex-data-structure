1ace117f600b01e7462e7fe666f1785148d36192
lucas ley <ayaaa168@163.com>
2021-12-07 22:37:16 +0800

    feat(BTree):add btree test

--- src/main/java/com/spacex/data/structure/btree/BTreeTest.java
+++ src/main/java/com/spacex/data/structure/btree/BTreeTest.java
@@ -3,6 +3,7 @@ package com.spacex.data.structure.btree;
 public class BTreeTest {
     public static void main(String[] args) {
         run();
+        build();
     }
 
     public static void run() {
@@ -15,5 +16,27 @@ public class BTreeTest {
         bTree.put("www.ts.edu", "128.112.136.17");
         bTree.put("www.daoye-tech.edu", "128.112.136.18");
         System.out.println(bTree.toString());
+
+        System.out.println("www.cs.princeton.edu: " + bTree.getValue("www.cs.princeton.edu"));
+        System.out.println("www.ts.edu:           " + bTree.getValue("www.ts.edu"));
+        System.out.println("www.daoye-tech.edu:   " + bTree.getValue("www.daoye-tech.edu"));
+
+        System.out.println(bTree.size());
+        System.out.println(bTree.height());
+        System.out.println();
+    }
+
+    public static void build() {
+        BTree<String, String> bTree = new BTree<>();
+        bTree.put("A1", "A-1");
+        bTree.put("A2", "A-2");
+        bTree.put("A3", "A-3");
+        bTree.put("A4", "A-4");
+        bTree.put("A5", "A-5");
+        bTree.put("A6", "A-6");
+        bTree.put("A7", "A-7");
+        bTree.put("A8", "A-8");
+        System.out.println(bTree.toString());
+
     }
 }

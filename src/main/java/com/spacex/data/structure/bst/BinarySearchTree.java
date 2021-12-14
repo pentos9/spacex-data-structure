9a8a697430801cd72cffdf98d8674cb12ed8adbb
lucas ley <ayaaa168@163.com>
2021-12-14 19:00:33 +0800

    feat(BST):add binary tree

---
+++ src/main/java/com/spacex/data/structure/bst/BinarySearchTree.java
@@ -0,0 +1,41 @@
+package com.spacex.data.structure.bst;
+
+public class BinarySearchTree<Key extends Comparable<Key>, Value> {
+    private Node root;
+
+    private class Node {
+        private Key key;
+        private Value val;
+        private Node left, right;
+        private int size;
+
+        public Node(Key key, Value val, int size) {
+            this.key = key;
+            this.val = val;
+            this.size = size;
+        }
+    }
+
+    public BinarySearchTree() {
+    }
+
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    public int size() {
+        return size(root);
+    }
+
+    private int size(Node x) {
+        if (x == null) {
+            return 0;
+        } else {
+            return x.size;
+        }
+    }
+
+    public boolean contains(Key key) {
+        return false;
+    }
+}

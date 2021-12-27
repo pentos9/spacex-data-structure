15a233716695883365a63a2bd5eff87f9265b2d0
lucas ley <ayaaa168@163.com>
2021-12-27 16:16:17 +0800

    feat(BST):add binanry search tree

--- src/main/java/com/spacex/data/structure/bst/BinarySearchTree.java
+++ src/main/java/com/spacex/data/structure/bst/BinarySearchTree.java
@@ -342,6 +342,20 @@ public class BinarySearchTree<Key extends Comparable<Key>, Value> {
     }
 
     private void keys(Node x, Queue<Key> queue, Key lo, Key hi) {
-        return;
+        if (x == null) {
+            return;
+        }
+        int cmplo = lo.compareTo(x.key);
+        int cmphi = hi.compareTo(x.key);
+
+        if (cmplo < 0) {
+            keys(x.left, queue, lo, hi);
+        }
+        if (cmplo <= 0 && cmphi >= 0) {
+            queue.add(x.key);
+        }
+        if (cmphi > 0) {
+            keys(x.right, queue, lo, hi);
+        }
     }
 }

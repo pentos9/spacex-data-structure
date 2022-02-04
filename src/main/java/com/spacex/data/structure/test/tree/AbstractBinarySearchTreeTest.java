19a554206d12f71c66e8595a7c40909ea1faf709
lucas ley <ayaaa168@163.com>
2022-02-04 12:05:53 +0800

    test(BinarySearchTree):add binanry search tree

---
+++ src/main/java/com/spacex/data/structure/test/tree/AbstractBinarySearchTreeTest.java
@@ -0,0 +1,29 @@
+package com.spacex.data.structure.test.tree;
+
+import com.spacex.data.structure.bst.BinarySearchTree;
+import org.junit.jupiter.api.Test;
+
+public abstract class AbstractBinarySearchTreeTest {
+
+    private BinarySearchTree tree;
+
+    @Test
+    public void testSize() {
+
+    }
+
+    @Test
+    public void testNull() {
+
+    }
+
+    @Test
+    public void testNonNull() {
+
+    }
+
+    @Test
+    public void testEmpty() {
+
+    }
+}

2dfff6f3bc30885e319dc5727b9668761337f4f8
lucas ley <ayaaa168@163.com>
2022-03-21 11:11:36 +0800

    feat(Heap):add min heap

--- src/main/java/com/spacex/data/structure/heap/MinHeap.java
+++ src/main/java/com/spacex/data/structure/heap/MinHeap.java
@@ -18,14 +18,38 @@ public class MinHeap<T extends Comparable<T>> {
     }
 
     public void checkSize() {
-
+        if (this.size > this.heap.length / 2) {
+            T[] newHeap = (T[]) Array.newInstance(type, INITIAL_SIZE * 2);
+            System.arraycopy(this.heap, 0, newHeap, 0, this.heap.length);
+            this.heap = newHeap;
+        }
     }
 
-    public T getParent(int index) {
-        return null;
+    public int getParent(int index) {
+        return (index - 1) / 2;
     }
 
     public void insert(T value) {
+        checkSize();
+        this.heap[this.size] = value;
+        buildHeap(this.size);
+        this.size++;
+    }
+
+    private void buildHeap(int index) {
+        if (index > 0) {
+
+            int parentIndex = getParent(index);
+            T parent = this.heap[parentIndex];
+            if (this.heap[index].compareTo(parent) < 0) {
+                swap(index, parentIndex);
+            }
+
+            buildHeap(index);
+        }
+    }
+
+    private void swap(int aIndex, int bIndex) {
 
     }
 

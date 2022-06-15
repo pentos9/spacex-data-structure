9789654761482349fd945cf5a002081673568c68
lucas ley <ayaaa168@163.com>
2022-06-15 09:44:54 +0800

    test(Tree):add same tree judgement test

---
+++ src/main/java/com/spacex/data/structure/test/tree/SameTreeTest.java
@@ -0,0 +1,41 @@
+package com.spacex.data.structure.test.tree;
+
+public class SameTreeTest {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+
+        if (p == null || q == null) {
+            return false;
+        }
+
+        if (p.val != q.val) {
+            return false;
+        }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+
+    public void isSameTree2(TreeNode p, TreeNode q) {
+
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}

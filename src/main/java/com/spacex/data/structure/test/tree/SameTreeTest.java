59bff6c47566c6653894354c973a665b83b2072e
lucas ley <ayaaa168@163.com>
2022-06-16 13:04:18 +0800

    test(Tree):add same tree unit test

--- src/main/java/com/spacex/data/structure/test/tree/SameTreeTest.java
+++ src/main/java/com/spacex/data/structure/test/tree/SameTreeTest.java
@@ -1,6 +1,28 @@
 package com.spacex.data.structure.test.tree;
 
+import java.util.ArrayDeque;
+
 public class SameTreeTest {
+
+    private class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if (p == null && q == null) {
             return true;
@@ -16,26 +38,65 @@ public class SameTreeTest {
         return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 
-    public void isSameTree2(TreeNode p, TreeNode q) {
+    public boolean isSameTree2(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
 
+        if (!check(p, q)) {
+            return false;
+        }
+
+        ArrayDeque<TreeNode> depP = new ArrayDeque<>();
+        ArrayDeque<TreeNode> depQ = new ArrayDeque<>();
+
+        depP.addLast(p);
+        depQ.addLast(q);
+
+        while (!depP.isEmpty()) {
+            p = depP.removeFirst();
+            q = depQ.removeFirst();
+
+            if (!check(p, q)) {
+                return false;
+            }
+
+            if (p != null) {
+                if (!check(p.left, q.left)) {
+                    return false;
+                }
+
+                if (p.left != null) {
+                    depP.addLast(p.left);
+                    depQ.addLast(q.left);
+                }
+
+                if (!check(p.right, q.right)) {
+                    return false;
+                }
+
+                if (p.right != null) {
+                    depP.addLast(p.right);
+                    depQ.addLast(q.right);
+                }
+            }
+        }
+
+        return true;
     }
-}
 
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
+    private boolean check(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
 
-    TreeNode() {
-    }
+        if (p == null || q == null) {
+            return false;
+        }
 
-    TreeNode(int val) {
-        this.val = val;
-    }
-
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
+        if (p.val != q.val) {
+            return false;
+        }
+        return true;
     }
 }

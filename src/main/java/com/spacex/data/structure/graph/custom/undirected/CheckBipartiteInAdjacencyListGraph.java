2202c36f7ae44a3723440cf6731d4495b5d31ef2
lucas ley <ayaaa168@163.com>
2022-06-30 07:28:50 +0800

    feat(Graph):check graph bipartite or not

--- src/main/java/com/spacex/data/structure/graph/custom/undirected/CheckBipartiteInAdjacencyListGraph.java
+++ src/main/java/com/spacex/data/structure/graph/custom/undirected/CheckBipartiteInAdjacencyListGraph.java
@@ -1,5 +1,8 @@
 package com.spacex.data.structure.graph.custom.undirected;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class CheckBipartiteInAdjacencyListGraph extends AbstractUndirectedGraph {
     enum Color {
         WHITE, RED, GREEN;
@@ -53,6 +56,55 @@ public class CheckBipartiteInAdjacencyListGraph extends AbstractUndirectedGraph 
                 return false;
             }// end else if
         }
+        return true;
+    }
+
+    public boolean isBipartiteWithBFS() {
+        // check if graph is empty
+        if (this.vertices == 0) {
+            return true;
+        }
+
+        Color[] colors = new Color[this.vertices];
+        for (int i = 0; i < this.vertices; i++) {
+            colors[i] = Color.WHITE;
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        // start coloring vertices , this code will handle the disconnected graph as well
+        // color the first vertex withe RED
+        for (int source = 0; source < this.vertices; source++) {
+            if (colors[source] == Color.WHITE) {
+                colors[source] = Color.RED;
+
+                // add it to queue for bfs
+                queue.offer(source);
+                while (!queue.isEmpty()) {
+                    int v = queue.poll();
+                    for (int i = 0; i < this.adjList[v].size(); i++) {
+                        int dest = this.adjList[v].get(i);
+                        if (colors[dest] == Color.WHITE) {
+                            //color is with the alternate color of vertex v
+                            if (colors[v] == Color.RED) {
+                                colors[dest] = Color.GREEN;
+                            } else if (colors[v] == Color.GREEN) {
+                                colors[dest] = Color.RED;
+                            }
+
+                            // add vertex dest to queue
+                            queue.add(dest);
+                        } else if (colors[v] == colors[dest]) {
+                            // means vertex v and dest are in same color, so graph is not bipartite
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+
+        // if here means graph is successfully colored in two color, RED & GREEN
+        // graph is bipartite
+
         return true;
     }
 }

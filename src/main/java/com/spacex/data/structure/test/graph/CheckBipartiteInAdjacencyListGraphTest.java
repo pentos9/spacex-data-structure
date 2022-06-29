8d8be9c18dcc91aaeec2ecf060d867e2df2049f2
lucas ley <ayaaa168@163.com>
2022-06-30 07:23:13 +0800

    test(Graph):refactor check graph bipartite or not

--- src/main/java/com/spacex/data/structure/test/graph/CheckBipartiteInAdjacencyListGraphTest.java
+++ src/main/java/com/spacex/data/structure/test/graph/CheckBipartiteInAdjacencyListGraphTest.java
@@ -1,26 +1,39 @@
 package com.spacex.data.structure.test.graph;
 
 import com.spacex.data.structure.graph.custom.undirected.CheckBipartiteInAdjacencyListGraph;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class CheckBipartiteInAdjacencyListGraphTest {
 
     @Test
     public void test() {
+        CheckBipartiteInAdjacencyListGraph graph = createNonBipartiteGraph();
+        Assertions.assertFalse(graph.isBipartite());
+    }
+
+    private CheckBipartiteInAdjacencyListGraph createNonBipartiteGraph() {
         CheckBipartiteInAdjacencyListGraph graph = new CheckBipartiteInAdjacencyListGraph(4);
         graph.addEdge(0, 1);
         graph.addEdge(0, 2);
         graph.addEdge(0, 3);
         graph.addEdge(2, 3);
         graph.addEdge(3, 1);
+        return graph;
+    }
 
-        System.out.println(graph.isBipartite());
+    private CheckBipartiteInAdjacencyListGraph createBipartiteGraph() {
+        CheckBipartiteInAdjacencyListGraph graph = new CheckBipartiteInAdjacencyListGraph(4);
+        graph.addEdge(0, 1);
+        graph.addEdge(0, 2);
+        graph.addEdge(2, 3);
+        graph.addEdge(3, 1);
+        return graph;
+    }
 
-        CheckBipartiteInAdjacencyListGraph graph2 = new CheckBipartiteInAdjacencyListGraph(4);
-        graph2.addEdge(0,1 );
-        graph2.addEdge(0,2 );
-        graph2.addEdge(2,3 );
-        graph2.addEdge(3,1 );
-        System.out.println(graph2.isBipartite());
+    @Test
+    public void testIsBipartiteWithBFS() {
+        CheckBipartiteInAdjacencyListGraph graph = createBipartiteGraph();
+        Assertions.assertTrue(graph.isBipartiteWithBFS());
     }
 }

4eb7e84c5a040a660aaffab859496f7c17ab0d1f
lucas ley <ayaaa168@163.com>
2022-07-07 09:46:25 +0800

    refactor(Graph):refeactor abstract undirected graph

--- src/main/java/com/spacex/data/structure/graph/custom/undirected/CheckIfEdgeIsBridgeGraph.java
+++ src/main/java/com/spacex/data/structure/graph/custom/undirected/CheckIfEdgeIsBridgeGraph.java
@@ -5,36 +5,6 @@ public class CheckIfEdgeIsBridgeGraph extends AbstractUndirectedGraph {
         super(vertices);
     }
 
-    public int countConnectedComponents() {
-        boolean[] visited = new boolean[this.vertices];
-        int count = 0;
-        int index;
-        while ((index = checkGraphIsVisited(visited)) != -1) {
-            dfs(index, visited);
-            count++;
-        }
-        return count;
-    }
-
-    public int checkGraphIsVisited(boolean[] visited) {
-        for (int i = 0; i < visited.length; i++) {
-            if (!visited[i]) {
-                return i;
-            }
-        }
-        return -1;
-    }
-
-    private void dfs(int start, boolean[] visited) {
-        visited[start] = true;
-        for (int i = 0; i < this.adjList[start].size(); i++) {
-            int destination = adjList[start].get(i);
-            if (!visited[destination]) {
-                dfs(destination, visited);
-            }
-        }
-    }
-
     public boolean edgeIsBridge(int source, int destination) {
         // count the connected components
         int count = countConnectedComponents();

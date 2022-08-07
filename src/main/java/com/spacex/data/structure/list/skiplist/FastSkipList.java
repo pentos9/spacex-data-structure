d74a25b7d1cc193a4e7e37d35be8e6a6fceba8d4
lucas ley <ayaaa168@163.com>
2022-08-07 10:48:59 +0800

    feat(SkipList):add skip list

--- src/main/java/com/spacex/data/structure/list/skiplist/FastSkipList.java
+++ src/main/java/com/spacex/data/structure/list/skiplist/FastSkipList.java
@@ -59,7 +59,32 @@ public class FastSkipList<T> {
     }
 
     public void print() {
+        SkipNode<T> tempNode = this.headNode;
+        int index = 1;
+        SkipNode<T> last = tempNode;
+        while (last != null) {
+            last = last.down;
+        }
 
+        while (tempNode != null) {
+            SkipNode<T> enumNode = tempNode.right;
+            SkipNode<T> enumLast = last.right;
+
+            while (enumLast != null && enumNode != null) {
+                if (enumLast.key == enumNode.key) {
+                    System.out.printf("%-5s", enumLast.key + "->");
+                    enumLast = enumLast.right;
+                    enumNode = enumNode.right;
+                } else {
+                    enumLast = enumLast.right;
+                    System.out.printf("%-s", "");
+                }
+            }
+
+            tempNode = tempNode.down;
+            index++;
+            System.out.println();
+        }
     }
 
     private static class SkipNode<T> {

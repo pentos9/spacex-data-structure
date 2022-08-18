65e1026b73e8cb6778631dc3e83f0dac95c4c750
lucas ley <ayaaa168@163.com>
2022-08-18 08:01:00 +0800

    test(CircleQueue):add circle queue unit test

--- src/main/java/com/spacex/data/structure/test/queue/ArrayCircleQueueTest.java
+++ src/main/java/com/spacex/data/structure/test/queue/ArrayCircleQueueTest.java
@@ -8,15 +8,29 @@ import java.util.UUID;
 
 public class ArrayCircleQueueTest {
 
-    @Test
-    public void test() {
-        int maxSize = 100;
+    private ArrayCircleQueue<String> produce(int maxSize) {
         ArrayCircleQueue<String> queue = new ArrayCircleQueue<>(maxSize);
         for (int i = 0; i < maxSize; i++) {
             String uuid = UUID.randomUUID().toString();
             queue.enQueue(uuid);
         }
+        return queue;
+    }
 
+    @Test
+    public void test() {
+        int maxSize = 100;
+        ArrayCircleQueue<String> queue = produce(maxSize);
         Assertions.assertEquals(maxSize - 1, queue.size());
+    }
+
+    @Test
+    public void testDequeue() {
+        int maxSize = 100;
+        ArrayCircleQueue<String> queue = produce(maxSize);
+        for (; !queue.isEmpty(); ) {
+            queue.deQueue();
+        }
+        Assertions.assertTrue(queue.isEmpty());
     }
 }
